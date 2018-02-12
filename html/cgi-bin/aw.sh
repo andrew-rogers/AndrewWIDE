12fd6ffd3eb83e63c58b2ddc98e31eb2305bc42d
Andrew Rogers <andrew.rogerstech@gmail.com>
2018-02-12 15:56:42 +0000

    Added CONTENT_LENGTH checking otherwise CGI script hangs when used with python http.server --cgi

--- html/cgi-bin/aw.sh
+++ html/cgi-bin/aw.sh
@@ -5,10 +5,22 @@
 
 echo "Content-type: text/plain"
 echo
-POST="$(head -n3)" # capture first three lines from stdin
+
+if [ -z "$CONTENT_LENGTH" ]; then
+  exit 0
+fi
+
+POST=""
+if [ "$CONTENT_LENGTH" -gt 0 ]; then
+  POST=$(dd bs=1 count=$CONTENT_LENGTH)
+fi
+
 SCR=$(echo -n "$POST" | head -n1) # first line
 STDIN=$(echo -n "$POST" | tail -n1) # third line
 STDOUT=$(echo -n "$STDIN" | base64 -d | eval "$SCR" | base64 | tr -d '\n')
 echo $?
 echo h2
 echo -n "$STDOUT"
+
+exit 0
+
