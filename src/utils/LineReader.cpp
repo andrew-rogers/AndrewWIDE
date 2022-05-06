d928db17294b7938c2b6acb4dd0e04f39e6e0ae3
Andrew Rogers <andrew.rogerstech@gmail.com>
2022-05-06 23:28:58 +0100

    Input sections document.

--- src/utils/LineReader.cpp
+++ src/utils/LineReader.cpp
@@ -1,10 +1,26 @@
 #include "LineReader.h"
 
+std::vector<Line> Line::split(const std::string& delim)
+{
+    std::vector<Line> ret;
+    std::size_t pos = 0U;
+    std::size_t found;
+    while ( (found = find(delim, pos)) != std::string::npos)
+    {
+        Line l( substr( pos, found-pos ) );
+        ret.push_back( l );
+        pos = found + delim.size();
+    }
+    Line l( substr( pos, delim.size()-pos ) );
+    ret.push_back( l );
+    return ret;
+}
+
 LineReader::LineReader(const std::string& input) : m_input(input), m_pos(0U), m_good(true)
 {
 }
 
-std::string LineReader::read()
+Line LineReader::read()
 {
     std::string line;
     std::size_t found = m_input.find("\n", m_pos);
--- src/utils/LineReader.h
+++ src/utils/LineReader.h
@@ -2,12 +2,22 @@
 #define LINEREADER_H
 
 #include <string>
+#include <vector>
+
+class Line : public std::string
+{
+public:
+    Line( const std::string& str) : std::string(str)
+    {
+    }
+    std::vector<Line> split(const std::string& delim);
+};
 
 class LineReader
 {
 public:
     LineReader(const std::string& input);
-    std::string read();
+    Line read();
     bool good() const
     {
         return m_good;
