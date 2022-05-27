2cb8185967b4b73420ce94e7258fa25f06735191
Andrew Rogers <andrew.rogerstech@gmail.com>
2022-05-27 17:45:20 +0100

    Move javascript from AwDocViewer.html to new AwDocViewer.js file.

---
+++ www/AwDocViewer.js
@@ -0,0 +1,88 @@
+/**
+ *
+ * @licstart  The following is the entire license notice for the 
+ *  JavaScript code in this page.
+ *
+ * Copyright (C) 2022  Andrew Rogers
+ *
+ *
+ * The JavaScript code in this page is free software: you can
+ * redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GNU GPL) as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option)
+ * any later version.  The code is distributed WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU GPL for more details.
+ *
+ * As additional permission under GNU GPL version 3 section 7, you
+ * may distribute non-source (e.g., minimized or compacted) forms of
+ * that code without the copy of the GNU GPL normally required by
+ * section 4, provided you include this license notice and a URL
+ * through which recipients can access the Corresponding Source.
+ *
+ * @licend  The above is the entire license notice
+ * for the JavaScript code in this page.
+ *
+ */
+
+function AwDocViewer() {
+    this._disableDrop();
+    this.ta_awjson = document.getElementById("ta_awjson");
+    if (this.ta_awjson == null) this._selectDoc();
+    else this._renderFromHTML;
+}
+
+AwDocViewer.prototype._disableDrop = function () {
+    // Prevent dropped files being openned in new browser tabs.
+    window.addEventListener("dragover",function(e){
+        e.preventDefault();
+    },false);
+    window.addEventListener("drop",function(e){
+        e.preventDefault();
+    },false);
+};
+
+AwDocViewer.prototype._instantiateRenderers = function () {
+    this.awdr = new AwDocRenderer(this.docname);
+    new AwCppRenderer(this.awdr);
+    var cppwasm = new AwCppWasmRenderer(this.awdr);
+    var xhr = new XhrRenderer("/cgi-bin/awcpp.cgi", this.awdr);
+    this.awdr.registerRenderer("awcppwasm", cppwasm);
+    this.awdr.registerRenderer("mjmd", new MathJaxMarkdownRenderer());
+    this.awdr.registerRenderer("diagram", new DiagramRenderer());
+    new PlotRenderer(this.awdr);
+};
+
+AwDocViewer.prototype._openDoc = function( fn ) {
+    this.docname = fn;
+    this._instantiateRenderers();
+    var that = this;
+    FileSystem.readFile(fn, function( err, content ) {
+        that.awdr.render(content);
+        that.awdr.start();
+    });
+};
+
+AwDocViewer.prototype._renderFromHTML = function( fn ) {
+    // TODO: Support rendering of AwJSON embedded in hidden textareas of HTML.
+};
+
+AwDocViewer.prototype._selectDoc = function( fn ) {
+    var fn = window.location.search;
+    if (fn.startsWith("?file=")) {
+        // Get filename from URL query.
+        var fn = decodeURIComponent(fn.slice(6));
+        this._openDoc(fn);
+    }
+    else {
+        // Create a fileselector to get filename from user.
+        div=document.createElement("div");
+        document.body.appendChild(div);
+        var fs = new FileSelector(div, FileSystem.list);
+        var that = this;
+        fs.show(function(fn){
+            that._openDoc(fn);
+        });
+    }
+};
+
