00e88fe6199f7c12b80932cc2be0a060c4a43672
Andrew Rogers <andrew.rogerstech@gmail.com>
2024-02-09 09:23:19 +0000

    Extract C++ from multiple awdocs and link into one wasm file.

---
+++ tools/aw2cpp.mjs
@@ -0,0 +1,54 @@
+/**
+ *
+ * @licstart  The following is the entire license notice for the 
+ *  JavaScript code in this page.
+ *
+ * Copyright (C) 2024  Andrew Rogers
+ *
+ *
+ * The JavaScript code in this page is free software: you can
+ * redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GNU GPL) as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option)
+ * any later version.  The code is distributed WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU GPL for more details.
+ *
+ * As additional permission under GNU GPL version 3 section 7, you
+ * may distribute non-source (e.g., minimized or compacted) forms of
+ * that code without the copy of the GNU GPL normally required by
+ * section 4, provided you include this license notice and a URL
+ * through which recipients can access the Corresponding Source.
+ *
+ * @licend  The above is the entire license notice
+ * for the JavaScript code in this page.
+ *
+ */
+
+import * as fs from 'fs'
+import {parseAwDoc} from "../modules/renderer.mjs"
+
+function aw2cpp(fn_in, fn_out, prefix) {
+    const input = fs.readFileSync(fn_in, 'utf8');
+    let doc = parseAwDoc(input);
+    let src = "#include \"wasm_buffers.h\"\n\n";
+    for (let i=0; i < doc.length; i++) {
+        if (doc[i].type == "awcppwasm") {
+            let id = doc[i].id || prefix + "_" + (i+1);
+            if (id == "globals") {
+                src += doc[i].content;
+            } else {
+                src += "EMSCRIPTEN_KEEPALIVE\n";
+                src += "extern \"C\" void " + id + "()\n{\n";
+                src += doc[i].content;
+                src += "}\n\n";
+            }
+        }
+    }
+    fs.writeFileSync(fn_out, src);
+}
+
+if (process.argv.length == 5) {
+    aw2cpp(process.argv[2], process.argv[3], process.argv[4]);
+}
+
